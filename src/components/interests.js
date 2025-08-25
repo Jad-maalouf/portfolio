7ca6732863b8d27fc9bfaea5da9908fb8459be43
Jad Maalouf <80184480+Jad-maalouf@users.noreply.github.com>
2025-08-25 16:44:54 +0300

    Update interests.js

--- src/components/interests.js
+++ src/components/interests.js
@@ -3,7 +3,7 @@ import React from "react";
 const Interests = () => {
   return (
     <section className="container section">
-      <h2>Interests & Activities</h2>
+      <h2>Interests and Activities</h2>
       <div className="interests-grid">
         <div className="interest">🩸 Active member in Lebanese Red Cross</div>
         <div className="interest">🎬 Watching movies and series</div>
